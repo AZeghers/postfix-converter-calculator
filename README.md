e33017631e3fda7019b04808894683f7bad9c719
azeghers <42282939+AZeghers@users.noreply.github.com>
2020-04-04 08:37:19 +0200

    start script and README test

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # postfix-converter-calculator
+
 Small program that converts an infix calculation into postfix and computes it
 
 ---
 
 ## Usage
-node index.js
+
+npm start
+
+![Console Usage](/public/images/calculator3000.png)
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const { computePostfix } = require('./calculator');
 
 const calculateInput = (input) => {
 	const postfix = infixToPostfix(input.toString());
-	if (postfix) console.log(computePostfix(postfix));
+	if (postfix) {
+		console.log('postfix =', postfix.join(' '));
+		console.log('result =', computePostfix(postfix));
+	}
 };
 
 (() => {
 	console.log('Calculator3000 (All Rights Reserved)');
 	console.log('Accepted operands are ( ) + - * /');
+	console.log();
 	console.log('Input a calculation:');
 	process.stdin.on('data', (input) => {
 		calculateInput(input);
